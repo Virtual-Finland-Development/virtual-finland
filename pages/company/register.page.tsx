37363fd6201a3365c9426e19077dc6e994c28043
Lauri Saarela <lauri.saarela@gofore.com>
2023-03-09 11:30:08 +0200

    removed unsued page

--- pages/company/register.page.tsx
+++
@@ -1,9 +0,0 @@
-import Page from '@/components/layout/page';
-
-export default function CompanyRegisterPage() {
-  return (
-    <Page title="Register company">
-      <div>terve</div>
-    </Page>
-  );
-}
